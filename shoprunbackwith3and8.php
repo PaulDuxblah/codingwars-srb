6ee761cf3580ada9ab91100082e72f1f1e281be4
PaulGirardin <paul@shoprunback.com>
2018-07-04 14:09:27 +0200

    add space

--- shoprunbackwith3and8.php
+++ shoprunbackwith3and8.php
@@ -2,7 +2,7 @@
 
 $array = range(1, 100);
 
-foreach($array as $key => $value) {
+foreach ($array as $key => $value) {
     if ($value % 3 == 0) {
         if ($value % 8 == 0) {
             $array[$key] = 'ShopRunBack';
